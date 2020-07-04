c713a4970c49de9caf77b114bdbb0f9fe54274b6
Anthony <verdes.anthony@gmail.com>
2020-07-03 21:31:53 -0500

    set up player input

---
+++ Assets/Rocket.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rocket : MonoBehaviour
+{
+    void Update()
+    {
+        ProcessInput();
+    }
+
+    private void ProcessInput()
+    {
+        if (Input.GetKey(KeyCode.Space)) 
+        {
+            print("space");
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            print("rotate left");
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            print("rotate right");
+        }
+    }
+}
